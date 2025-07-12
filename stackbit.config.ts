7a8559363424f2913e3fbe39ad25f3558389edd0
qazizubairr <qazizubair471@gmail.com>
2025-07-12 11:18:11 +0500

    changed structure

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,26 +1,37 @@
 import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from "@stackbit/cms-git";
+import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  nodeVersion: "18",
-  ssgName: "custom", // You can leave this if you're not using an official SSG integration
+  stackbitVersion: '~0.6.0',
+  nodeVersion: '18',
+  ssgName: 'custom', // "custom" is okay for Astro
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
-      contentDirs: ["src"],
+      contentDirs: ['content'],
       models: [
         {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "src/pages/{slug}.json",
+          name: 'Page',
+          type: 'page',
+          label: 'Pages',
+          urlPath: '/{slug}',
+          filePath: 'content/pages/{slug}.json',
           fields: [
-            { name: "title", type: "string", required: true }
-          ]
-        }
+            {
+              name: 'title',
+              type: 'string',
+              required: true,
+            },
+            {
+              name: 'markdown_content',
+              label: 'Content',
+              type: 'markdown',
+              required: false,
+            },
+          ],
+        },
       ],
-    })
+    }),
   ],
-  postInstallCommand: "npm i --no-save @stackbit/types"
+  postInstallCommand: 'npm i --no-save @stackbit/types',
 });
